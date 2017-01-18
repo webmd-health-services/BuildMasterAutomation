40c9c373a7de29417f6a05cdec683c2c08aad520
Aaron Jensen <ajensen@webmd.net>
2017-01-17 17:09:20 -0800

    Creating initial BuildMasterAutomation module.

---
+++ BuildMasterAutomation/Import-BuildMasterAutomation.ps1
@@ -0,0 +1,7 @@
+
+if( (Get-Module -Name 'BuildMasterAutomation') )
+{
+    Remove-Module -Name 'BuildMasterAutomation' -Force
+}
+
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'BuildMasterAutomation.psd1' -Resolve)
