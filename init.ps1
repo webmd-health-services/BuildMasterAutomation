3112842d8b35d0d249e9051aa98cbda152c267d3
Aaron Jensen <ajensen@webmd.net>
2017-01-24 17:03:55 -0800

    Adding more debugging code to figure out why BuildMaster isn't installing on Appveyor.

--- init.ps1
+++ init.ps1
@@ -38,16 +38,36 @@ if( -not $bmInstallInfo )
         $connString = $appVeyorConnString
     }
 
+    $outputRoot = Join-Path -Path $PSScriptRoot -ChildPath '.output'
+    New-Item -Path $outputRoot -ItemType 'Directory' -ErrorAction Ignore
+
+    $logRoot = Join-Path -Path $outputRoot -ChildPath 'logs'
+    New-Item -Path $logRoot -ItemType 'Directory' -ErrorAction Ignore
+
     Write-Verbose -Message ('Running BuildMaster installer {0}.' -f $installerPath)
-    $logPath = Join-Path -Path $PSScriptRoot -ChildPath 'buildmaster.install.log'
-    $process = Start-Process -FilePath $installerPath -ArgumentList '/S','/Edition=Express',('/ConnectionString={0}' -f $connString),('/LogFile={0}' -f $logPath) -Wait -PassThru
+    $logPath = Join-Path -Path $logRoot -ChildPath 'buildmaster.install.log'
+    $installerFileName = $installerPath | Split-Path -Leaf
+    $stdOutLogPath = Join-Path -Path $logRoot -ChildPath ('{0}.stdout.log' -f $installerFileName)
+    $stdErrLogPath = Join-Path -Path $logRoot -ChildPath ('{0}.stderr.log' -f $installerFileName)
+    $process = Start-Process -FilePath $installerPath `
+                             -ArgumentList '/S','/Edition=Express',('/ConnectionString={0}' -f $connString),('/LogFile={0}' -f $logPath) `
+                             -Wait `
+                             -PassThru `
+                             -RedirectStandardError $stdErrLogPath `
+                             -RedirectStandardOutput $stdOutLogPath
     $process.WaitForExit()
+
+    Write-Verbose -Message ('{0} exited with code {1}' -f $installerFileName,$process.ExitCode)
 
     if( -not (Get-ProgramInstallInfo -Name 'BuildMaster') )
     {
         if( (Test-Path -Path 'env:APPVEYOR') )
         {
-            Get-Content -Path $logPath | Write-Output
+            Get-ChildItem -Path $logRoot |
+                ForEach-Object {
+                    $_
+                    $_ | Get-Content -Path $logPath
+                }
         }
         Write-Error -Message ('It looks like BuildMaster {0} didn''t install. The install log might have more information: {1}' -f $version,$logPath)
     }
