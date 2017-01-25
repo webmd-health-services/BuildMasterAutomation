e8cbc997eaf55897b43d18ec18eb456ec14630fe
Aaron Jensen <ajensen@webmd.net>
2017-01-24 17:32:05 -0800

    Upload test results so they show on AppVeyor dashboard.

--- build.ps1
+++ build.ps1
@@ -11,9 +11,20 @@ $outputdir = Join-Path -Path $PSScriptRoot -ChildPath '.output'
 New-Item -Path $outputdir -ItemType 'Directory' -ErrorAction Ignore
 Get-ChildItem -Path $outputdir | Remove-Item -Recurse -Force
 
+$outputFile = Join-Path -Path $outputdir -ChildPath 'pester.xml'
 $result = Invoke-Pester -Script (Join-Path -Path $PSScriptRoot -ChildPath 'Tests\*.Tests.ps1') `
-                        -OutputFile (Join-Path -Path $outputdir -ChildPath 'pester.xml') `
+                        -OutputFile $outputFile `
                         -OutputFormat NUnitXml `
                         -PassThru
+
+if( (Test-Path -Path 'env:APPVEYOR') )
+{
+    [xml]$content = Get-Content -Path $outputFile
+    $content.'test-results'.'test-suite'.type = "Powershell"
+    $content.Save($outputFile)
+
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $outputFile)
+}
 
 exit $result.FailedCount
