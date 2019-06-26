c8165a202fb7cfbc8c14b7b732daa6a0b32ffc02
Aaron Jensen <ajensen@webmd.net>
2019-06-26 16:29:31 -0700

    Typo

--- BuildMasterAutomation/Functions/New-BMServerRole.ps1
+++ BuildMasterAutomation/Functions/New-BMServerRole.ps1
@@ -8,7 +8,7 @@ function New-BMServerRole
     .DESCRIPTION
     The `New-BMServerRole` creates a new server role in BuildMaster. Pass the name of the role to the `Name` parameter. Names may only contain letters, numbers, spaces, periods, underscores, or dashes.
     
-    Every role must have a unique name. If you creat a role with a duplicate name, you'll get an error.
+    Every role must have a unique name. If you create a role with a duplicate name, you'll get an error.
     
     This function uses BuildMaster's infrastructure management API.
 
