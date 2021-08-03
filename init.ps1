aa23ace8f440ac1acd5776ef1fff4b355eae5369
Aaron Jensen <ajensen@webmd.net>
2021-08-02 17:57:20 -0700

    Traditional installer fails to get a license key from Inedo.

--- init.ps1
+++ init.ps1
@@ -13,114 +13,62 @@ If BuildMaster is already installed, nothing happens.
 #>
 [CmdletBinding()]
 param(
+    [String] $SqlServerName
 )
 
-#Requires -RunAsAdministrator
 #Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 $ProgressPreference = [Management.Automation.ActionPreference]::SilentlyContinue
-
-& {
-    $VerbosePreference = 'SilentlyContinue'
-    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'PSModules\Carbon') -Force
-    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'PSModules\SqlServer') -Force
-}
+$InformationPreference = [Management.Automation.ActionPreference]::Continue
 
 $runningUnderAppVeyor = (Test-Path -Path 'env:APPVEYOR')
 
 $version = '6.1.28'
 Write-Verbose -Message ('Testing BuildMaster {0}' -f $version)
-$sqlServer = $null
-$installerPath = 'SQL'
-$installerUri = 'sql'
-$dbParam = '/InstallSqlExpress'
-
-$sqlServers = @()
-if( (Test-Path -Path 'env:APPVEYOR') )
+$credentials = 'Integrated Security=True'
+if( -not $SqlServerName )
 {
-    $sqlServers = Get-Item -Path ('SQLSERVER:\SQL\{0}\SQL2016' -f [Environment]::MachineName)
-}
-else
-{
-    $sqlServers = Get-ChildItem -Path ('SQLSERVER:\SQL\{0}' -f [Environment]::MachineName)
-}
-
-foreach( $item in $sqlServers )
-{
-    if( $item.Status -ne [Microsoft.SqlServer.Management.Smo.ServerStatus]::Online )
+    if( $runningUnderAppVeyor )
     {
-        Write-Verbose -Message ('Skipping SQL Server instance "{0}": "{1}".' -f $item.Name,$item.Status)
-        continue
+        $SqlServerName = '.\SQL2016'
+        $credentials = 'User ID=sa;Password=Password12!'
     }
-
-    $item | Format-List | Out-String | Write-Verbose
-
-    if( -not $item.InstanceName -or $item.InstanceName -in @( 'Inedo', 'SQL2016' ) )
+    else
     {
-        Write-Verbose -Message ('Found SQL Server instance "{0}": "{1}".' -f $item.Name,$item.Status)
-        $installerPath = 'NO{0}' -f $installerPath
-        $installerUri = 'no{0}' -f $installerUri
-        $sqlServer = $item
-        $credentials = 'Integrated Security=true;'
-        if( $runningUnderAppVeyor )
-        {
-            $credentials = 'User ID=sa;Password=Password12!'
-        }
-        $dbParam = '"/ConnectionString=Server={0};Database=BuildMaster;{1}"' -f $sqlServer.Name,$credentials
-        break
+        $SqlServerName = '.'
     }
 }
 
-$installerPath = Join-Path -Path $PSScriptRoot -ChildPath ('.output\BuildMasterInstaller{0}-{1}.exe' -f $installerPath,$version)
-$installerUri = 'https://my.inedo.com/services/legacy/downloads/buildmaster/{0}/{1}.exe' -f $installerUri,$version
+$connString = "Server=$($SqlServerName);Database=BuildMaster;$($credentials)"
 
-if( -not (Test-Path -Path $installerPath -PathType Leaf) )
+# Always install the latest Hub.
+$hubDirPath = Join-Path -Path $PSScriptRoot -ChildPath '.output\InedoHub'
+$hubPath = Join-Path -Path $hubDirPath -ChildPath 'hub.exe'
+if( (Test-Path -Path $hubDirPath) )
 {
-    Write-Verbose -Message ('Downloading {0}' -f $installerUri)
-    Invoke-WebRequest -Uri $installerUri -OutFile $installerPath
+    Remove-Item -Path $hubDirPath -Recurse -Force -ErrorAction Stop
+}
+New-Item -Path $hubDirPath -ItemType 'Directory' -Force | Out-Null
+
+$hubZipPath = Join-Path -Path ($hubDirPath | Split-Path) -ChildPath 'InedoDesktopHub.zip'
+$hubZipUrl = 'https://proget.inedo.com/upack/Products/download/InedoReleases/DesktopHub?contentOnly=zip&latest'
+
+Invoke-WebRequest -Uri $hubZipUrl -OutFile $hubZipPath
+Expand-Archive -Path $hubZipPath -DestinationPath $hubDirPath
+
+$installedApps = "`n" | & $hubPath 'list'
+if( ($installedApps | Select-String "BuildMaster $($version)" -SimpleMatch) )
+{
+    Write-Information "BuildMaster already installed.`n$($installedApps)"
+    exit 0
 }
 
-$bmInstallInfo = Get-ProgramInstallInfo -Name 'BuildMaster'
-if( -not $bmInstallInfo )
+& $hubPath 'install' "BuildMaster:$($version)" --ConnectionString=$connString
+
+if( (Get-Service -Name 'INEDOBM*' | Measure-Object).Count -ne 2 )
 {
-    $outputRoot = Join-Path -Path $PSScriptRoot -ChildPath '.output'
-    New-Item -Path $outputRoot -ItemType 'Directory' -ErrorAction Ignore
-
-    $logRoot = Join-Path -Path $outputRoot -ChildPath 'logs'
-    New-Item -Path $logRoot -ItemType 'Directory' -ErrorAction Ignore
-
-    Write-Verbose -Message ('Running BuildMaster installer {0}.' -f $installerPath)
-    $logPath = Join-Path -Path $logRoot -ChildPath 'buildmaster.install.log'
-    $installerFileName = $installerPath | Split-Path -Leaf
-    $stdOutLogPath = Join-Path -Path $logRoot -ChildPath ('{0}.stdout.log' -f $installerFileName)
-    $stdErrLogPath = Join-Path -Path $logRoot -ChildPath ('{0}.stderr.log' -f $installerFileName)
-    $argumentList = '/S','/Edition=Express',$dbParam,('"/LogFile={0}"' -f $logPath)
-    Write-Verbose ('{0} {1}' -f $installerPath,($argumentList -join ' '))
-    $process = Start-Process -FilePath $installerPath `
-                             -ArgumentList $argumentList `
-                             -Wait `
-                             -PassThru `
-                             -RedirectStandardError $stdErrLogPath `
-                             -RedirectStandardOutput $stdOutLogPath
-    $process.WaitForExit()
-
-    Write-Verbose -Message ('{0} exited with code {1}' -f $installerFileName,$process.ExitCode)
-
-    if( -not (Get-ProgramInstallInfo -Name 'BuildMaster') )
-    {
-        if( $runningUnderAppVeyor )
-        {
-            Get-ChildItem -Path $logRoot |
-                ForEach-Object {
-                    $_
-                    $_ | Get-Content
-                }
-        }
-        Write-Error -Message ('It looks like BuildMaster {0} didn''t install. The install log might have more information: {1}' -f $version,$logPath)
-    }
-}
-elseif( $bmInstallInfo.DisplayVersion -notmatch ('^{0}\b' -f [regex]::Escape($version)) )
-{
-    Write-Warning -Message ('You''ve got an old version of BuildMaster installed. You''re on version {0}, but we expected version {1}. Please *completely* uninstall version {0} using the Programs and Features control panel, then re-run this script.' -f $bmInstallInfo.DisplayVersion,$version)
+    Write-Error -Message 'It looks like BuildMaster wasn''t installed. Didn''t find the two BuildMaster services.'
+    exit 1
 }
 
+exit 0
