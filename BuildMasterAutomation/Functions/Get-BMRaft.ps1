dc0007241800987cf81348390ad660e18f4d349a
Aaron Jensen <ajensen@webmd.net>
2022-10-25 12:31:33 -0700

    Adding complete support for creating, reading, updating, and deleting rafts.

--- BuildMasterAutomation/Functions/Get-BMRaft.ps1
+++ BuildMasterAutomation/Functions/Get-BMRaft.ps1
@@ -3,26 +3,82 @@ function Get-BMRaft
 {
     <#
     .SYNOPSIS
-    Gets all the rafts from BuildMaster.
+    Gets rafts from BuildMaster.
 
     .DESCRIPTION
-    The `Get-BMRaft` function returns all rafts from BuildMaster. It uses the native API.
+    The `Get-BMRaft` function returns all rafts from BuildMaster.
+
+    To get a specific raft, pass its name, id, or raft object to the `Raft` parameter (or pipe them into the function).
+    If a raft with the given ID or represented by the object doesn't exist, the function writes an error and returns.
+    If a string is passed to the `Raft` parameter, and it contains wildcards, the function will return all rafts whose
+    names match the wildcard pattern. Otherwise, it will return the raft with that name and if it doesn't find a raft
+    with that name, it writes an error and returns nothing.
+
+    This function uses the native API.
 
     .EXAMPLE
     Get-BMRaft -Session $session
 
-    Demonstrates how to use `Get-BMRaft` to get all rafts from BuildMaster.
+    Demonstrates how to use `Get-BMRaft` to get all rafts.
     #>
     [CmdletBinding()]
     param(
-        # A session object that represents the BuildMaster instance to use. Use the `New-BMSession` function to create
-        # session objects.
+        # The session to BuildMaster. Use the `New-BMSession` function to create a session.
         [Parameter(Mandatory)]
-        [Object] $Session
+        [Object] $Session,
+
+        # The raft id, name, or raft object to get. If you pass a string, and the string has wildcards, all rafts whose
+        # name matches the wildcard pattern are returned.
+        [Parameter(ValueFromPipeline)]
+        [Object] $Raft
     )
 
-    Set-StrictMode -Version 'Latest'
-    Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    process
+    {
+        Set-StrictMode -Version 'Latest'
+        Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+        $WhatIfPreference = $false  # We only get stuff in this function.
 
-    Invoke-BMNativeApiMethod -Session $Session -Name 'Rafts_GetRafts'
+        $searching = ($Raft | Test-BMName) -and [wildcardpattern]::ContainsWildcardCharacters($Raft)
+        $raftName = $Raft | Get-BMObjectName -ObjectTypeName 'Raft' -ErrorAction Ignore
+
+        $endpointName = 'Rafts_GetRaft'
+        if ($searching -or -not $Raft)
+        {
+            $endpointName = 'Rafts_GetRafts'
+        }
+
+        $parameters = @{}
+        if ($endpointName -eq 'Rafts_GetRaft')
+        {
+            $raftID = $Raft | Get-BMObjectID -ObjectTypeName 'Raft' -ErrorAction Ignore
+            if ($raftID)
+            {
+                $parameters['Raft_Id'] = $raftID
+            }
+            elseif ($raftName)
+            {
+                $parameters['Raft_Name'] = $raftName
+            }
+        }
+
+        $rafts = @()
+        Invoke-BMNativeApiMethod -Session $Session -Name $endpointName -Method Post -Parameter $parameters |
+            Where-Object {
+                if ($searching)
+                {
+                    return $_.Raft_Name -like $raftName
+                }
+
+                return $true
+            } |
+            Tee-Object -Variable 'rafts' |
+            Write-Output
+
+        if ($Raft -and -not $rafts -and -not $searching)
+        {
+            $msg = "Raft ""$($Raft | Get-BMObjectName)"" does not exist."
+            Write-Error -Message $msg -ErrorAction $ErrorActionPreference
+        }
+    }
 }
---
+++ Tests/Get-BMRaft.Tests.ps1
@@ -0,0 +1,118 @@
+
+#Requires -Version 5.1
+Set-StrictMode -Version 'Latest'
+
+BeforeAll {
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Tests.ps1' -Resolve)
+
+    $script:session = New-BMTestSession
+
+    function GivenRaft
+    {
+        param(
+            [Parameter(Mandatory)]
+            [String] $Named,
+
+            [switch] $PassThru
+        )
+
+        $raft = $null
+        $Named | Set-BMRaft -Session $script:session -PassThru:$PassThru | Tee-Object -Variable 'raft'
+
+        if ($PassThru)
+        {
+            $raft | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    function ThenReturned
+    {
+        [CmdletBinding()]
+        param(
+            [Parameter(Mandatory, ParameterSetName='ByCount')]
+            [int] $Count,
+
+            [Parameter(Mandatory, ParameterSetName='ByName')]
+            [String] $RaftNamed
+        )
+
+        if ($PSCmdlet.ParameterSetName -eq 'ByCount')
+        {
+            $script:results | Should -HaveCount $Count
+            return
+        }
+
+        $script:results | Should -Not -BeNullOrEmpty
+        $script:results | Where-Object 'Raft_Name' -EQ $RaftNamed | Should -Not -BeNullOrEmpty
+    }
+
+    function WhenGettingRafts
+    {
+        [CmdletBinding()]
+        param(
+            [hashtable] $WithArgs = @{}
+        )
+
+        $script:results = Get-BMRaft -Session $script:session @WithArgs
+    }
+}
+
+Describe 'Get-BMRaft' {
+    BeforeEach {
+        Get-BMRaft -Session $script:session |
+            Where-Object 'Raft_Name' -NE 'Default' |
+            Remove-BMRaft -Session $script:session
+        Get-BMRaft -Session $script:session | Should -HaveCount 1
+        $Global:Error.Clear()
+    }
+
+    It 'should get all rafts' {
+        GivenRaft 'all rafts'
+        WhenGettingRafts
+        ThenReturned -Count 2
+        ThenReturned -RaftNamed 'Default'
+        ThenReturned -RaftNamed 'all rafts'
+    }
+
+    It 'should get raft by name' {
+        GivenRaft 'named raft'
+        WhenGettingRafts -WithArgs @{ 'Raft' = 'named raft' }
+        ThenReturned -Count 1
+        ThenReturned -RaftNamed 'named raft'
+    }
+
+    It 'should get raft by wildcard' {
+        GivenRaft 'wildcard raft'
+        WhenGettingRafts -WithArgs @{ 'Raft' = 'wildcard*' }
+        ThenReturned -Count 1
+        ThenReturned -RaftNamed 'wildcard raft'
+    }
+
+    It 'should write error for non-existent raft <_> ' -TestCases @('i do not exist', 9009) {
+        WhenGettingRafts -WithArgs @{ 'Raft' = $_ } -ErrorAction SilentlyContinue
+        ThenREturned -Count 0
+        ThenError ([regex]::Escape('does not exist'))
+    }
+
+    It 'should ignore errors for non-existent raft <_>' -TestCases @('i do not exist', 9009) {
+        WhenGettingRafts -WithArgs @{ 'Raft' = $_ ; ErrorAction = 'Ignore' }
+        ThenReturned -Count 0
+        ThenError -IsEmpty
+    }
+
+    It 'should get raft by id' {
+        $raft = GivenRaft 'id raft' -PassThru
+        WhenGettingRafts -WithArgs @{ 'Raft' = $raft.Raft_Id }
+        ThenReturned -Count 1
+        ThenReturned -RaftNamed 'id raft'
+    }
+
+    It 'should ignore whatif' {
+        GivenRaft 'whatif raft'
+        $WhatIfPreference = $true
+        WhenGettingRafts -WithArgs @{ 'Raft' = 'whatif raft' }
+        ThenReturned -Count 1
+        ThenReturned -RaftNamed 'whatif raft'
+        ThenError -IsEmpty
+    }
+}
