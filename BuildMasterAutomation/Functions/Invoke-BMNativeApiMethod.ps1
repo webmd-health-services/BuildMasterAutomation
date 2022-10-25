67b5c3e6d19de3d6131c779b9858d78efd302daa
Aaron Jensen <ajensen@webmd.net>
2022-10-25 13:20:02 -0700

    Formatting and removing PSScriptAnalyzer warning.

--- BuildMasterAutomation/Functions/Invoke-BMNativeApiMethod.ps1
+++ BuildMasterAutomation/Functions/Invoke-BMNativeApiMethod.ps1
@@ -8,7 +8,8 @@ function Invoke-BMNativeApiMethod
     .DESCRIPTION
     The `Invoke-BMNativeApiMethod` calls a method on BuildMaster's "native" API. From Inedo:
 
-    > This API endpoint should be avoided if there is an alternate API endpoint available, as those are much easier to use and will likely not change.
+    > This API endpoint should be avoided if there is an alternate API endpoint available, as those are much easier to
+    > use and will likely not change.
 
     In other words, use a native API at your own peril.
 
@@ -17,27 +18,28 @@ function Invoke-BMNativeApiMethod
     .EXAMPLE
     Invoke-BMNativeApiMethod -Session $session -Name 'Applications_CreateApplication' -Parameter @{ Application_Name = 'fubar' }
 
-    Demonstrates how to call `Invoke-BMNativeApiMethod`. In this example, it is calling the `Applications_CreateApplication` method to create a new application named `fubar`.
+    Demonstrates how to call `Invoke-BMNativeApiMethod`. In this example, it is calling the
+    `Applications_CreateApplication` method to create a new application named `fubar`.
     #>
-    [CmdletBinding(SupportsShouldProcess=$true)]
+    [Diagnostics.CodeAnalysis.SuppressMessage('PSShouldProcess', '')]
+    [CmdletBinding(SupportsShouldProcess)]
     param(
-        [Parameter(Mandatory=$true)]
-        [object]
-        # A session object that represents the BuildMaster instance to use. Use the `New-BMSession` function to create session objects.
-        $Session,
+        # The session to BuildMaster. Use `New-BMSession` to create a session.
+        [Parameter(Mandatory)]
+        [Object] $Session,
 
-        [Parameter(Mandatory=$true)]
-        [string]
-        # The name of the API method to use. The list can be found at http://inedo.com/support/documentation/buildmaster/reference/api/native, or under your local BuildMaster instance at /reference/api
-        $Name,
+        # The name of the API method to use. The list can be found at
+        # http://inedo.com/support/documentation/buildmaster/reference/api/native, or under your local BuildMaster
+        # instance at /reference/api
+        [Parameter(Mandatory)]
+        [String] $Name,
 
-        [Microsoft.PowerShell.Commands.WebRequestMethod]
         # The HTTP/web method to use. The default is `GET`.
-        $Method = [Microsoft.PowerShell.Commands.WebRequestMethod]::Get,
+        [Microsoft.PowerShell.Commands.WebRequestMethod] $Method =
+            [Microsoft.PowerShell.Commands.WebRequestMethod]::Get,
 
-        [hashtable]
         # Any parameters to pass to the endpoint. The keys/values are sent in the body of the request as a JSON object.
-        $Parameter
+        [hashtable] $Parameter
     )
 
     Set-StrictMode -Version 'Latest'
